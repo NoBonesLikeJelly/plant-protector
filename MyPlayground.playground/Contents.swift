6b0a0abd97d094ce72aa464f4b1535f5e31bb430
NoBonesLikeJelly <noboneslikejelly@gmail.com>
2016-04-16 18:45:37 +1200

    Update to playground test

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,6 +10,15 @@ import Darwin
 var str: String = "Hello, playground"
 let tst: String = "This will not change"
 let pi = 3.141592
+let conversion: Double = 400
 print("Pi is equal to \(pi)")
 let numberOfSteps: Int = 450
 print("Number of steps: \(numberOfSteps)")
+let maxInt: Int = 9223372036854775807
+print("Max Float: \(FLT_MAX)")
+print("Max Double: \(DBL_MAX)")
+let isOver20: Bool = true
+let statement = "my name is" + tst
+print(statement)
+print("I'm an Int now!!! \(Int(conversion))")
+
