90f018555c50d743a8e2eae4aa484795cd70907a
NoBonesLikeJelly <noboneslikejelly@gmail.com>
2016-04-16 13:21:20 +1200

    Test playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+/*
+ Multiline comments :D
+ */
+
+
+import UIKit
+import Darwin
+
+var str: String = "Hello, playground"
+let tst: String = "This will not change"
+let pi = 3.141592
+print("Pi is equal to \(pi)")
+let numberOfSteps: Int = 450
+print("Number of steps: \(numberOfSteps)")
