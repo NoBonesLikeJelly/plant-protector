9a3fc85d0307b577c0ead0b4abb923af5a912d21
NoBonesLikeJelly <noboneslikejelly@gmail.com>
2016-04-18 11:36:03 +1200

    Updtes to myplayground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,6 +8,7 @@ import UIKit
 import Darwin
 
 var str: String = "Hello, playground"
+let finnAge: Int = 11
 let tst: String = "This will not change"
 let pi = 3.141592
 let conversion: Double = 400
@@ -21,4 +22,21 @@ let isOver20: Bool = true
 let statement = "my name is" + tst
 print(statement)
 print("I'm an Int now!!! \(Int(conversion))")
+print("Finn is \(finnAge) years old.")
+
+let age: Int = 18
+
+if (age < 21) && (age > 17){
+    
+    print("You can drink alcahol but not drive")
+
+}else if (age < 18){
+ 
+    print("You cannot drink alcahol")
+
+}else{
+
+    print("You can have one drink and still drive")
+
+}
 
